07fb85d91ee48c4d3030ba09e102ab6c08af8fed
hongnikai <541737239@qq.com>
2020-07-15 10:56:18 +0800

    add

--- src/test/java/com/lc/interviewTest/jdbc/JDBCTest.java
+++ src/test/java/com/lc/interviewTest/jdbc/JDBCTest.java
@@ -14,34 +14,36 @@ public class JDBCTest {
         // Driver是一个接口,参数传递:MySQL驱动程序的实现类
         // DriverManager.registerDriver(new Driver());
         // 查看驱动类源码,注册两次驱动,浪费资源
-        Class.forName("com.mysql.jdbc.Driver");
+       /** Class.forName("com.mysql.jdbc.Driver");*/
         // 2. 获得连接
         // uri:数据库地址 jdbc:mysql://连接主机ip:端口号//数据库名字
-        String url = "jdbc:mysql://114.116.154.251:3306/choujiang";
+        /**String url = "jdbc:mysql://114.116.154.251:3306/choujiang";*/
         // static Connection getConnection(String url, String user, String password)
         // 返回值是java.sql.Connection接口的实现类,在MySQL驱动程序中
-        Connection conn = DriverManager.getConnection(url, "root", "123456");
-        System.out.println(conn);// com.mysql.jdbc.JDBC4Connection@10d1f30
+        /**Connection conn = DriverManager.getConnection(url, "root", "123456");*/
+        /**System.out.println(conn);// com.mysql.jdbc.JDBC4Connection@10d1f30*/
         // 3. 获得语句执行平台,通过数据库连接对象,获取到SQL语句的执行者对象
         //conn对象,调用方法 Statement createStatement() 获取Statement对象,将SQL语句发送到数据库
         //返回的是Statement接口的实现类对象,在MySQL驱动程序中
-        Statement stat = conn.createStatement();
-        System.out.println(stat);//com.mysql.jdbc.StatementImpl@137bc9
+        /**Statement stat = conn.createStatement();*/
+        /**System.out.println(stat);*///com.mysql.jdbc.StatementImpl@137bc9
         // 4. 执行sql语句
         //通过执行者对象调用方法执行SQL语句,获取结果
         //int executeUpdate(String sql)  执行数据库中的SQL语句,仅限于insert,update,delete
         //返回值int,操作成功数据库的行数
 //        int row = stat.executeUpdate("select * from t1");
-       ResultSet rs = stat.executeQuery("select * from t1");
-        while (rs.next()) {
-            // 获取每列的数据,使用的是ResultSet接口的方法getXXX
-            String xname = rs.getString("xname");
-            System.out.println(xname);
+       /*ResultSet rs = stat.executeQuery("select * from t1");*/
+       /* while (rs.next()) {*/
+       /*     // 获取每列的数据,使用的是ResultSet接口的方法getXXX*/
+       /*     String xname = rs.getString("xname");*/
+       /*     System.out.println(xname);*/
+/*
+*/
 
-        }
-        // 5. 释放资源
-        stat.close();
-        conn.close();
+       /* }*/
+       /* // 5. 释放资源*/
+       /* stat.close();*/
+       /* conn.close();*/
 
     }
 
